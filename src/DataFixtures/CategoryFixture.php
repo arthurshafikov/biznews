3ecc6f12d78326c51aa3b13a236625fa7597f456
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-23 07:36:37 +0400

    seed fake categories

---
+++ src/DataFixtures/CategoryFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixture extends Fixture
+{
+    public const CATEGORIES = [
+        'Business',
+        'Sport',
+        'Finance',
+        'Politics',
+    ];
+
+    public function load(ObjectManager $manager): void
+    {
+        foreach (static::CATEGORIES as $id => $categoryName) {
+             $category = new Category();
+             $category->setName($categoryName);
+             $category->setSlug(strtolower($categoryName));
+             $manager->persist($category);
+
+             $this->setReference('categories_' . $id, $category);
+        }
+
+        $manager->flush();
+    }
+}
