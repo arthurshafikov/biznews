9a9e49fcb733d6817e1f8720774dd555caf30990
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-25 07:02:07 +0400

    add posts/index page

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,13 +3,24 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Repository\PostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PostController extends AbstractController
 {
-    #[Route('/post/{post}', name: 'app_post')]
+    #[Route('/posts', name: 'app_posts')]
+    public function index(PostRepository $postRepository): Response
+    {
+        $posts = $postRepository->getLatestPosts(8);
+
+        return $this->render('post/index.html.twig', [
+            'posts' => $posts,
+        ]);
+    }
+
+    #[Route('/posts/{post}', name: 'app_post')]
     public function show(Post $post): Response
     {
         return $this->render('post/single.html.twig', [
