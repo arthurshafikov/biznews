bc1df21ce6c699fb899f7370ec62a6e24f7a6058
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-25 11:20:23 +0400

    fix breaking news component on the single post page

--- src/Components/BreakingNewsComponent.php
+++ src/Components/BreakingNewsComponent.php
@@ -2,9 +2,18 @@
 
 namespace App\Components;
 
+use App\Repository\PostRepository;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('breaking-news')]
 class BreakingNewsComponent
 {
+    public function __construct(private readonly PostRepository $postRepository)
+    {
+    }
+
+    public function getBreakingNews(): array
+    {
+        return $this->postRepository->getTrendingPosts(3);
+    }
 }
