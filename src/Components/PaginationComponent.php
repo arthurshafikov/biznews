c347c4627f881ec1fa6839f03e8f1fa12ef4c006
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-25 07:39:26 +0400

    add pagination component

---
+++ src/Components/PaginationComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Components;
+
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('pagination')]
+class PaginationComponent
+{
+    public int $lastPage;
+    public string $paginationPath;
+    public bool $showAlwaysFirstAndLast = true;
+}
