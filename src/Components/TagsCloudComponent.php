6a58dc744bc05f866d03df066e39e23869765984
arthurshafikov <arthur.shafikov27@gmail.com>
2022-09-26 06:38:50 +0400

    fetch tags in tags cloud in sidebar

--- src/Components/TagsCloudComponent.php
+++ src/Components/TagsCloudComponent.php
@@ -2,9 +2,18 @@
 
 namespace App\Components;
 
+use App\Repository\TagRepository;
 use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 
 #[AsTwigComponent('tags-cloud')]
 class TagsCloudComponent
 {
+    public function __construct(public readonly TagRepository $tagRepository)
+    {
+    }
+
+    public function getTags(): array
+    {
+        return $this->tagRepository->findAll();
+    }
 }
