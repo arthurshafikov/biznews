ccef590f7aff721922f202c6621241b773570353
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-03 07:11:47 +0300

    redirect to the login page after logout

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -47,7 +47,6 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
             return new RedirectResponse($targetPath);
         }
 
-        // For example:
          return new RedirectResponse($this->urlGenerator->generate('app_home'));
     }
 
