dacd5fd5f8ec43df41ccd4e6296e921bb182e2f1
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-06 07:21:23 +0300

    add SubscribedEmailCreated event

---
+++ src/Events/SubscribedEmailCreated.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Events;
+
+use App\Entity\SubscribedEmail;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class SubscribedEmailCreated extends Event
+{
+    public const NAME = 'subscribe-email.created';
+
+    public function __construct(public readonly SubscribedEmail $subscribedEmail)
+    {
+    }
+}
--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,20 @@
 
 namespace App;
 
+use App\Events\SubscribedEmailCreated;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\EventDispatcher\DependencyInjection\AddEventAliasesPass;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container)
+    {
+        $container->addCompilerPass(new AddEventAliasesPass([
+            SubscribedEmailCreated::class => SubscribedEmailCreated::NAME,
+        ]));
+    }
 }
