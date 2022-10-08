e056898947f132d697e0cf8d1a5e3356257a3916
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-08 08:15:35 +0300

    add comment/add route

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Comment;
+use App\Entity\Post;
+use App\Repository\CommentRepository;
+use DateTime;
+use DateTimeImmutable;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    #[Route('/posts/{slug}/comment', name: 'app_comment')]
+    public function comment(Post $post, Request $request, CommentRepository $commentRepository): Response
+    {
+        // user unauth middleware
+
+        $comment = new Comment();
+        $comment->setPost($post);
+        $comment->setUser($this->getUser());
+        $comment->setParent($commentRepository->find($request->get('parent_id')));
+        $comment->setContent($request->get('content'));
+        $comment->setCreatedAt(DateTimeImmutable::createFromMutable(new DateTime()));
+
+        $commentRepository->add($comment, true);
+
+        $request->getSession()->getFlashBag()->add('session-message',  [
+            'message' => 'Comment added successfully!',
+        ]);
+
+        return $this->redirectToRoute('app_post', [
+            'slug' => $post->getSlug(),
+        ]);
+    }
+}
