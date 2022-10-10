a5dda88c1b1c4743bf1b4899b141abb7f092e0f3
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-10 07:58:14 +0300

    generate subscription verification url in listener

--- src/Listeners/SendSubscriptionVerificationEmail.php
+++ src/Listeners/SendSubscriptionVerificationEmail.php
@@ -2,11 +2,14 @@
 
 namespace App\Listeners;
 
+use App\Entity\Setting;
 use App\Events\SubscribedEmailCreated;
+use App\Repository\SettingRepository;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\EventDispatcher\Attribute\AsEventListener;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Throwable;
 use Twig\Environment;
 
@@ -15,6 +18,8 @@ class SendSubscriptionVerificationEmail
 {
     public function __construct(
         private readonly MailerInterface $mailer,
+        private readonly UrlGeneratorInterface $urlGenerator,
+        private readonly SettingRepository $settingRepository,
         private readonly LoggerInterface $logger,
         private readonly Environment $twig
     ) {
@@ -23,9 +28,11 @@ class SendSubscriptionVerificationEmail
     public function onSubscribedEmailCreated(SubscribedEmailCreated $event): void
     {
         try {
-            $emailHTML = $this->twig->render('emails/subscription-verification.html.twig', [
-                'email' => $event->subscribedEmail->getEmail(),
-                'token' => $event->subscribedEmail->getToken(),
+            $emailHTML = $this->twig->render('emails/email-verification.html.twig', [
+                'url' => $this->urlGenerator->generate('app_newsletter_confirm', [
+                    'email' => $event->subscribedEmail->getEmail(),
+                    'token' => $event->subscribedEmail->getToken(),
+                ], UrlGeneratorInterface::ABSOLUTE_URL)
             ]);
         } catch (Throwable $e) {
             $this->logger->error($e->getMessage());
@@ -33,7 +40,7 @@ class SendSubscriptionVerificationEmail
         }
 
         $email = (new Email())
-            ->from('main@biznews.com') // todo config?
+            ->from($this->settingRepository->getSettingValue(Setting::COMPANY_EMAIL))
             ->to($event->subscribedEmail->getEmail())
             ->subject('Please verify your subscription!')
             ->html($emailHTML);
