244a88138977f4901f306c1f654073fdb81f0826
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-10 07:23:16 +0300

    add get-in-touch component

---
+++ src/Components/GetInTouchComponent.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Components;
+
+use App\Entity\Setting;
+use App\Repository\SettingRepository;
+use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
+
+#[AsTwigComponent('get-in-touch')]
+class GetInTouchComponent
+{
+    public function __construct(private readonly SettingRepository $settingRepository) {}
+
+    public function __call(string $name, array $arguments): ?string
+    {
+        return $this->settingRepository->getSettingValue(constant(Setting::class . '::' . $name));
+    }
+}
