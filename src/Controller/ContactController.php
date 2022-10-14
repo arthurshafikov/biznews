132c3aa7f896442b94163bb0f1ada14b27722f83
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-14 08:24:18 +0300

    add ContactFormType

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -2,16 +2,24 @@
 
 namespace App\Controller;
 
+use App\Form\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ContactController extends Controller
+class ContactController extends AbstractController
 {
-    #[Route('/contact', name: 'app_contact')]
+    #[Route('/contact', name: 'app_contact', methods: ['GET'])]
     public function index(): Response
     {
+        return $this->returnContactFormResponse($this->createForm(ContactFormType::class));
+    }
+
+    private function returnContactFormResponse(FormInterface $form): Response
+    {
         return $this->render('contact/index.html.twig', [
-            'controller_name' => 'ContactController',
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/ContactFormType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 1, 'max' => 15]),
+                ],
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Your Name',
+                    'class' => 'form-control p-4',
+                ],
+            ])
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Email(),
+                ],
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Your Email',
+                    'class' => 'form-control p-4',
+                ],
+            ])
+            ->add('subject', TextType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 1, 'max' => 20]),
+                ],
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Subject',
+                    'class' => 'form-control p-4',
+                ],
+            ])
+            ->add('message', TextareaType::class, [
+                'constraints' => [
+                    new NotBlank(),
+                    new Length(['min' => 5, 'max' => 2000]),
+                ],
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Message',
+                    'rows' => 4,
+                    'class' => 'form-control',
+                ],
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // todo csrf
+        ]);
+    }
+}
