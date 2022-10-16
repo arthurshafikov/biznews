8bbcc8d11a95c0f681c739b273169aaad17a0f4d
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-16 07:06:48 +0300

    post comment with ajax request

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -3,8 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
-use App\Entity\Post;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
+use App\Repository\PostRepository;
 use DateTime;
 use DateTimeImmutable;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -14,24 +15,32 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class CommentController extends AbstractController
 {
-    #[Route('/posts/{slug}/comment', name: 'app_comment')]
-    public function comment(Post $post, Request $request, CommentRepository $commentRepository): Response
-    {
-        $comment = new Comment();
-        $comment->setPost($post);
-        $comment->setUser($this->getUser());
-        $comment->setParent($commentRepository->find($request->get('parent_id')));
-        $comment->setContent($request->get('content'));
-        $comment->setCreatedAt(DateTimeImmutable::createFromMutable(new DateTime()));
+    #[Route('/posts/comment', name: 'app_comment')]
+    public function comment(
+        Request $request,
+        CommentRepository $commentRepository,
+        PostRepository $postRepository,
+    ): Response {
+        $commentForm = $this->createForm(CommentFormType::class);
+        $commentForm->handleRequest($request);
+        if ($commentForm->isSubmitted() && $commentForm->isValid()) {
+            $commentData = $commentForm->getData();
+            $comment = new Comment();
+            $comment->setPost($postRepository->find($commentData['post']));
+            $comment->setUser($this->getUser());
+            $comment->setParent($commentData['parent'] ? $commentRepository->find($commentData['parent']) : null);
+            $comment->setContent($commentData['content']);
+            $comment->setCreatedAt(DateTimeImmutable::createFromMutable(new DateTime()));
 
-        $commentRepository->add($comment, true);
+            $commentRepository->add($comment, true);
 
-        $request->getSession()->getFlashBag()->add('session-message', [
-            'message' => 'Comment added successfully!',
-        ]);
+            $request->getSession()->getFlashBag()->add('session-message', [
+                'message' => 'Comment added successfully!',
+            ]);
+        }
 
-        return $this->redirectToRoute('app_post', [
-            'slug' => $post->getSlug(),
+        return $this->render('post/comment-form.html.twig', [
+            'commentForm' => $commentForm->createView(),
         ]);
     }
 }
