0e8d34dcf78671583db20e957b9d2ca990f949a4
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-16 08:08:55 +0300

    move registration logic from controller to UserService

--- src/Controller/Auth/RegisterController.php
+++ src/Controller/Auth/RegisterController.php
@@ -2,74 +2,48 @@
 
 namespace App\Controller\Auth;
 
-use App\Events\UserChangedEmail;
 use App\Form\RegistrationFormType;
-use App\Service\TokenGeneratorService;
-use DateTime;
-use DateTimeImmutable;
-use Doctrine\Persistence\ManagerRegistry;
+use App\Service\UserService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
-    public function __construct(
-        private readonly EventDispatcherInterface $eventDispatcher,
-        private readonly TokenGeneratorService $tokenGeneratorService
-    ) {
-    }
-
     #[Route('/register', name: 'app_register_show', methods: ['GET'])]
     public function showRegistrationForm(): Response
     {
-        return $this->render('auth/register.html.twig', [
-            'form' => $this->createForm(RegistrationFormType::class)->createView(),
-        ]);
+        return $this->returnRegisterFormResponse($this->createForm(RegistrationFormType::class));
     }
 
     #[Route('/register', name: 'app_register', methods: ['POST'])]
-    public function register(
-        Request $request,
-        ManagerRegistry $doctrine,
-        UserPasswordHasherInterface $hasher,
-    ): Response {
-        $entityManager = $doctrine->getManager();
-
-        $userForm = $this->createForm(RegistrationFormType::class);
-        $userForm->handleRequest($request);
-        if (!$userForm->isValid()) {
-            return $this->render('auth/register.html.twig', [
-                'form' => $userForm->createView(),
-            ]);
+    public function register(Request $request, UserService $userService): Response
+    {
+        $registrationForm = $this->createForm(RegistrationFormType::class);
+        $registrationForm->handleRequest($request);
+        if (!$registrationForm->isSubmitted() || !$registrationForm->isValid()) {
+            return $this->returnRegisterFormResponse($registrationForm);
         }
 
-        /**
-         * @var $user User
-         */
-        $user = $userForm->getData();
-        $user->setVerified(false);
-        $user->setPassword($hasher->hashPassword($user, $user->getPassword()));
-        $user->setCreatedAt(DateTimeImmutable::createFromMutable(new DateTime()));
-
-        $entityManager->persist($user);
-        $entityManager->flush();
+        $userService->register([
+            'name' => $registrationForm->get('name')->getData(),
+            'email' => $registrationForm->get('email')->getData(),
+            'password' => $registrationForm->get('password')->getData(),
+        ]);
 
         $request->getSession()->getFlashBag()->add('session-message', [
             'message' => 'You have registered successfully',
         ]);
 
-        $this->eventDispatcher->dispatch(
-            new UserChangedEmail(
-                $user,
-                $this->tokenGeneratorService->generateToken($user->getEmail())
-            ),
-            UserChangedEmail::NAME
-        );
-
         return $this->redirectToRoute('app_login');
+    }
+
+    private function returnRegisterFormResponse(FormInterface $form): Response
+    {
+        return $this->render('auth/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
     }
 }
---
+++ src/Service/UserService.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\User;
+use App\Events\UserChangedEmail;
+use App\Repository\UserRepository;
+use DateTime;
+use DateTimeImmutable;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserService
+{
+    public function __construct(
+        private readonly UserRepository $userRepository,
+        private readonly UserPasswordHasherInterface $hasher,
+        private readonly EventDispatcherInterface $eventDispatcher,
+        private readonly TokenGeneratorService $tokenGeneratorService,
+    ) {
+    }
+
+    public function register(array $params): void
+    {
+        $user = new User();
+        $user->setName($params['name']);
+        $user->setEmail($params['email']);
+        $user->setVerified(false);
+        $user->setPassword($this->hasher->hashPassword($user, $params['password']));
+        $user->setCreatedAt(DateTimeImmutable::createFromMutable(new DateTime()));
+
+        $this->eventDispatcher->dispatch(
+            new UserChangedEmail(
+                $user,
+                $this->tokenGeneratorService->generateToken($user->getEmail())
+            ),
+            UserChangedEmail::NAME
+        );
+
+        $this->userRepository->add($user, true);
+    }
+}
