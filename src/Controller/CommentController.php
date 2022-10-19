cca9b57546969ef408cff31408ee86cd0cdd6459
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-19 07:45:20 +0300

    do not allow unverified users to comment

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -12,14 +12,25 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     #[Route('/posts/comment', name: 'app_comment')]
-    public function comment(
-        Request $request,
-        CommentService $commentService,
-    ): Response {
+    public function comment(Request $request, CommentService $commentService): Response
+    {
         $commentForm = $this->createForm(CommentFormType::class);
         $commentForm->handleRequest($request);
+
+        $user = $this->getUser();
+        if (!$user->isVerified()) {
+            $request->getSession()->getFlashBag()->add('session-message', [
+                'message' => 'Please verify your email!',
+                'type' => 'error',
+            ]);
+
+            return $this->render('post/comment-form.html.twig', [
+                'commentForm' => $commentForm->createView(),
+            ]);
+        }
+
         if ($commentForm->isSubmitted() && $commentForm->isValid()) {
-            $commentService->add($commentForm->getData(), $this->getUser());
+            $commentService->add($commentForm->getData(), $user);
             $request->getSession()->getFlashBag()->add('session-message', [
                 'message' => 'Comment added successfully!',
             ]);
