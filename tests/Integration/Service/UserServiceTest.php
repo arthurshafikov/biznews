a9569e681ef6397abc963e6c38c9c92d64899c2d
arthurshafikov <arthur.shafikov27@gmail.com>
2022-10-20 07:32:58 +0300

    add UserService verifyToken() tests

--- tests/Integration/Service/UserServiceTest.php
+++ tests/Integration/Service/UserServiceTest.php
@@ -1,9 +1,11 @@
 <?php
 
-namespace App\Tests;
+namespace App\Tests\Integration\Service;
 
 use App\Entity\User;
+use App\EntityFactory\UserFactory;
 use App\Repository\UserRepository;
+use App\Service\TokenGeneratorService;
 use App\Service\UserService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -43,10 +45,40 @@ class UserServiceTest extends KernelTestCase
         $user = $userRepository->findOneBy([
             'email' => $params['email'],
         ]);
-
         $this->assertSame('New User', $user->getName());
         $this->assertSame('test@email.com', $user->getEmail());
         $this->assertFalse($user->isVerified());
+    }
+
+    public function testVerifyToken(): void
+    {
+        $container = static::getContainer();
+        /** @var User $user */
+        $user = $container->get(UserFactory::class)->createFake();
+        $mockTokenGeneratorService = $this->createMock(TokenGeneratorService::class);
+        $mockTokenGeneratorService
+            ->expects(self::once())
+            ->method('generateToken')
+            ->willReturn('someGeneratedToken');
+        $container->set(TokenGeneratorService::class, $mockTokenGeneratorService);
+        /** @var UserService $userService */
+        $userService = $container->get(UserService::class);
+
+        $result = $userService->verifyToken($user->getEmail(), 'someGeneratedToken');
+
+        $this->assertTrue($result);
+        $this->assertTrue($user->isVerified());
+    }
+
+    public function testVerifyTokenUserNotExists(): void
+    {
+        $container = static::getContainer();
+        /** @var UserService $userService */
+        $userService = $container->get(UserService::class);
+
+        $result = $userService->verifyToken('non-exists@email.com', 'uselessToken');
+
+        $this->assertFalse($result);
     }
 
     private function truncateEntities(array $entities)
